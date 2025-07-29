79e5a8276d68f7742b6d54698e150047afb471e7
Pavel Egorov <pe.xoposhiy@gmail.com>
2025-07-29 12:06:10 +0200

    Show "My Levels" topic in SidebarNavigation only in debug mode

--- web/src/components/SidebarNavigationContainer.tsx
+++ web/src/components/SidebarNavigationContainer.tsx
@@ -3,6 +3,7 @@ import {useNavigate} from 'react-router-dom';
 import {GameStateContext} from '../reducers';
 import {loadLevel} from '../reducers/actionCreators';
 import {TopicItem} from './TopicItem';
+import {isDebugModeEnabled} from '../utils/debugUtils';
 
 /**
  * Sidebar navigation component showing topics and levels
@@ -53,33 +54,35 @@ export function SidebarNavigationContainer(): React.ReactElement {
                     />
                 ))}
 
-                {/* My Levels topic */}
-                <div className="">
-                    <div
-                        className="flex items-center p-1 cursor-pointer"
-                        onClick={() => toggleTopic('My Levels')}
-                    >
-                        <span className="mr-1 font-mono">
-                          {expandedTopics['My Levels'] ? '-' : '+'}
-                        </span>
-                        <span>My Levels</span>
-                    </div>
-
-                    {expandedTopics['My Levels'] && (
-                        <div>
-                            {/* Create New button */}
-                            <div
-                                className="flex items-center pl-4 p-1 cursor-pointer hover:bg-[#37373d]"
-                                onClick={handleCreateNewLevel}
-                            >
-                                <span className="mr-2">+</span>
-                                <span>Create New</span>
-                            </div>
-
-                            {/* Render My Levels here when implemented */}
+                {/* My Levels topic - only shown in debug mode */}
+                {isDebugModeEnabled() && (
+                    <div className="">
+                        <div
+                            className="flex items-center p-1 cursor-pointer"
+                            onClick={() => toggleTopic('My Levels')}
+                        >
+                            <span className="mr-1 font-mono">
+                              {expandedTopics['My Levels'] ? '-' : '+'}
+                            </span>
+                            <span>My Levels</span>
                         </div>
-                    )}
-                </div>
+
+                        {expandedTopics['My Levels'] && (
+                            <div>
+                                {/* Create New button */}
+                                <div
+                                    className="flex items-center pl-4 p-1 cursor-pointer hover:bg-[#37373d]"
+                                    onClick={handleCreateNewLevel}
+                                >
+                                    <span className="mr-2">+</span>
+                                    <span>Create New</span>
+                                </div>
+
+                                {/* Render My Levels here when implemented */}
+                            </div>
+                        )}
+                    </div>
+                )}
 
                 {state.topics.length === 0 && (
                     <div className="p-2 text-[#888888]">Loading...</div>
